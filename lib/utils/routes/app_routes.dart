cba2b8a1c5d92e98aaec2f2a07b2d4246ab079af
sparkwud <kelvinagbenyo@gmail.com>
2024-05-25 00:57:03 +0000

    configure app routing using go router

--- lib/utils/routes/app_routes.dart
+++ lib/utils/routes/app_routes.dart
@@ -7,4 +7,20 @@ class AppRoutes {
   static const String interest = '/interest';
 
   static const String home = '/home';
+
+  static const String navBar = '/navBar';
+
+  static const String comingSoon = '/comingSoon';
+
+  static const String lesson = '/lesson';
+
+  static const String exercise = '/exercise';
+
+  static const String games = '/games';
+
+  static const String chats = '/chats';
+
+  static const String streaks = '/streaks';
+
+  static const String speaking = '/speaking';
 }
--- lib/utils/routes/router.dart
+++ lib/utils/routes/router.dart
@@ -1,6 +1,11 @@
 import 'package:go_router/go_router.dart';
-import 'package:speak_sphere/features/home/views/home_screen.dart';
-import 'package:speak_sphere/features/home/views/interests_screen.dart';
+import 'package:speak_sphere/features/home/views/screens/bottom_nav_bar.dart';
+import 'package:speak_sphere/features/home/views/screens/coming_soon_screen.dart';
+import 'package:speak_sphere/features/home/views/screens/home_screen.dart';
+import 'package:speak_sphere/features/home/views/screens/interests_screen.dart';
+import 'package:speak_sphere/features/home/views/screens/speaking_screen.dart';
+import 'package:speak_sphere/features/home/views/screens/streaks_screen.dart';
+import 'package:speak_sphere/features/lessons/views/screens/lesson_screen.dart';
 import 'package:speak_sphere/features/splash/views/splash_screen.dart';
 
 import 'app_routes.dart';
@@ -19,6 +24,26 @@ final GoRouter appRouter = GoRouter(
     GoRoute(
       path: AppRoutes.home,
       builder: (context, state) => const HomeScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.navBar,
+      builder: (context, state) => const BottomNavBar(),
+    ),
+    GoRoute(
+      path: AppRoutes.comingSoon,
+      builder: (context, state) => const ComingSoonScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.lesson,
+      builder: (context, state) => const LessonScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.streaks,
+      builder: (context, state) => const StreaksScreen(),
+    ),
+    GoRoute(
+      path: AppRoutes.speaking,
+      builder: (context, state) => const SpeakingScreen(),
     ),
   ],
 );
